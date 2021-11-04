259c35f4345ea7c8bc45d1ce617bb5bb2da5197f
Dzenovich <zakirov.zrz@gmail.com>
2021-11-04 14:07:16 +0300

    new1

---
+++ task3209/HTMLFileFilter.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
+
+public class HTMLFileFilter extends FileFilter {
+    @Override
+    public boolean accept(File file) {
+       return ((file.isDirectory()))
+               || (file.getName().toLowerCase().endsWith(".html"))
+               || (file.getName().toLowerCase().endsWith(".htm"));
+    }
+
+    public HTMLFileFilter() {
+        super();
+    }
+
+    @Override
+    public String getDescription() {
+        return "HTML и HTM файлы";
+    }
+}
